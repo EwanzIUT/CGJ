4bddc564111aa4151ecad2c0433b55ceb541c5ad
AugustinFrey <augustin.frey@etu.umontpellier.fr>
2020-01-22 06:03:09 +0100

    Augustin Menus

---
+++ Augustin/TestMenu/Assets/Timer_Pres.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer_Pres : MonoBehaviour
+{
+
+    public float TempsRestant = 18.0f;
+    public bool fin = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TempsRestant -= Time.deltaTime;
+        if(TempsRestant < 0)
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+}
